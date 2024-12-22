034b1186210a971ec84379f9f73c394493a03fb1
supakawaiidesu <kishanamin97@gmail.com>
2024-12-22 02:36:12 -0600

    better lev slider design

--- src/components/features/trading/OrderCard/components/MarketOrderForm.tsx
+++ src/components/features/trading/OrderCard/components/MarketOrderForm.tsx
@@ -122,24 +122,33 @@ export function MarketOrderForm({
             </div>
           </div>
           <div className="space-y-1">
-            <Slider
-              value={[Number(tempLeverage)]}
-              min={1}
-              max={100}
-              step={1}
-              onValueChange={(value) => {
-                setTempLeverage(value[0].toString());
-              }}
-              onValueCommit={(value) => {
-                onLeverageChange(value[0].toString());
-              }}
-            />
-            <div className="flex justify-between px-1 text-xs text-muted-foreground">
-              <span>1x</span>
-              <span>25x</span>
-              <span>50x</span>
-              <span>75x</span>
-              <span>100x</span>
+            <div className="relative w-full">
+              <div className="absolute flex justify-between w-full pointer-events-none -top-1 -bottom-1">
+                <div className="w-[3px] h-full rounded-full opacity-20 bg-primary"></div>
+                <div className="w-[3px] h-full rounded-full opacity-20 bg-primary"></div>
+                <div className="w-[3px] h-full rounded-full opacity-20 bg-primary"></div>
+                <div className="w-[3px] h-full rounded-full opacity-20 bg-primary"></div>
+                <div className="w-[3px] h-full rounded-full opacity-20 bg-primary"></div>
+              </div>
+              <Slider
+                value={[Number(tempLeverage)]}
+                min={1}
+                max={100}
+                step={1}
+                onValueChange={(value) => {
+                  setTempLeverage(value[0].toString());
+                }}
+                onValueCommit={(value) => {
+                  onLeverageChange(value[0].toString());
+                }}
+              />
+            </div>
+            <div className="relative w-full h-4">
+              <div className="absolute left-0 text-xs -translate-x-1/2 text-muted-foreground">1x</div>
+              <div className="absolute left-[25%] -translate-x-1/2 text-xs text-muted-foreground">25x</div>
+              <div className="absolute left-[50%] -translate-x-1/2 text-xs text-muted-foreground">50x</div>
+              <div className="absolute left-[75%] -translate-x-1/2 text-xs text-muted-foreground">75x</div>
+              <div className="absolute right-0 text-xs translate-x-1/2 text-muted-foreground">100x</div>
             </div>
           </div>
         </div>
